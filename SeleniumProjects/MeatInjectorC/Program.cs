ef9a3981dc06e891a60323936ff9b698225fd38d
Enrique Alberto Navia <anavia_n@outlook.com>
2022-08-27 19:35:40 -0500

    Add MeatInjector to the solution

---
+++ SeleniumProjects/MeatInjectorC/Program.cs
@@ -0,0 +1,16 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+
+namespace MeatInjectorC
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver MeatObject = new ChromeDriver();
+            MeatObject.Navigate().GoToUrl("https://tinder.com/");
+
+        }
+    }
+}
